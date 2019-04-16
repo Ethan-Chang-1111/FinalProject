28806dee06adf570e5b6a70566ae88def7cbda65
Matthew Harkey <mharkey01@gmail.com>
2019-04-16 11:44:41 -0500

    why

--- FinalProject/GameScene.swift
+++ FinalProject/GameScene.swift
@@ -10,14 +10,13 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    //help meeeeeee
+    //helpmeeeeeee
     private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var spinnyNode: SKShapeNode?
     
     override func didMove(to view: SKView) {
         
-        
-        // Get label node from scene and store it for use later
+        //Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
             label.alpha = 0.0
