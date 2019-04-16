bb878614d608557a26c52040b0f978b49d3fc11d
Patrick Dowell <patrick.dowell@comcast.net>
2019-04-16 11:12:35 -0500

    big daddy

--- FinalProject/GameScene.swift
+++ FinalProject/GameScene.swift
@@ -16,6 +16,8 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         //testing
+        //big daddy
+        
         
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
