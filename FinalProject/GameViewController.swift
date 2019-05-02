b3cc6f8de01084d98ba0cec895b93c3ab62ad240
Matthew Harkey <mharkey01@gmail.com>
2019-05-02 11:35:47 -0500

    Changed Order of Scenes

--- FinalProject/GameViewController.swift
+++ FinalProject/GameViewController.swift
@@ -16,8 +16,8 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            // Load the SKScene from 'TitleScreen.sks'
+            if let scene = SKScene(fileNamed: "TitleScreen") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
