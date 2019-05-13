6e1f52f67b0375cee187afeb1de8d15c07a6179d
Matthew Harkey <mharkey01@gmail.com>
2019-05-13 11:08:17 -0500

    test

--- FinalProject/TitleScreen.swift
+++ FinalProject/TitleScreen.swift
@@ -12,7 +12,7 @@ import SpriteKit
 class TitleScreen: SKScene {
     
     override func didMove(to view: SKView) {
-        self.backgroundColor = SKColor.green
+        createBackground()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -21,4 +21,21 @@ class TitleScreen: SKScene {
         self.view?.presentScene(gameScreen!, transition: SKTransition.fade(withDuration: 0.5 ))
         
     }
+    func createBackground() {
+        let rain = SKTexture(imageNamed: "betterBackground")
+        for i in 0...1 {
+            let rainBackground = SKSpriteNode(texture: rain)
+            rainBackground.zPosition = -1
+            rainBackground.position = CGPoint(x: 0, y: rainBackground.size.height * CGFloat(i))
+            addChild(rainBackground)
+            let moveDown = SKAction.moveBy(x: 0, y: -rainBackground.size.height, duration: 20)
+            let moveReset = SKAction.moveBy(x: 0, y: rainBackground.size.height, duration: 0)
+            let moveLoop = SKAction.sequence([moveDown, moveReset])
+            let moveForever = SKAction.repeatForever(moveLoop)
+            rainBackground.run(moveForever)
+        }
+    }
+
 }
+
+
